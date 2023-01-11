2280041c8c943a7f9f29104e709f33f48a0e6177
SainiBhavesh <bhaveshsaini2002@gmail.com>
2023-01-11 18:38:59 +0530

    changes in api

--- function.py
+++ function.py
@@ -13,31 +13,80 @@ def submit(src:str,dest:str):
     price=flightScrape(src,dest)   
     price = price.split('\n')
     
-    # print(src)
+    # print(price)
+
     
     final_data=[]
-    for i in range(0,len(price)):
-        data={}
-        if  src in price[i].lower():
-            data['company']=price[i-3]
-            # print(company)
-            data['flightNo']=price[i-2]
-            # print(flightNo)
-            data['depTime']=price[i-1]
-            # print(depTime)
-            data['source']=price[i]
-            # print(source)
-            data['duration']=price[i+1]
-            # print(duration)
-            data['type']=price[i+2]
-            # print(type)
-            data['arrivalTime']=price[i+3]
-            # print(arrivalTime)
-            data['destination']=price[i+4]
-            # print(destination)
-            data['rate']=price[i+5]
-            # print(rate)
-            print("\n")
+    # for i in range(0,len(price)):
+    #     data={}
+    #     if  src in price[i].lower():
+    #         data['company']=price[i-3]
+    #         # print(company)
+    #         data['flightNo']=price[i-2]
+    #         # print(flightNo)
+    #         data['depTime']=price[i-1]
+    #         # print(depTime)
+    #         data['source']=price[i]
+    #         # print(source)
+    #         data['duration']=price[i+1]
+    #         # print(duration)
+    #         data['type']=price[i+2]
+    #         # print(type)
+            
+    #         if price[i+4]=='+ 1 DAY':
+    #             data['arrivalTime']=price[i+3]+price[i+4]
+    #             # print(arrivalTime)
+    #             data['destination']=price[i+5]
+    #             # print(destination)
+    #             data['rate']=price[i+6]
+    #             # print(rate)
+    #         else:
+    #             data['arrivalTime']=price[i+3]
+    #             # print(arrivalTime)
+    #             data['destination']=price[i+4]
+    #              # print(destination)
+    #             data['rate']=price[i+5]
+    #             # print(rate)
+            
+    #         print("\n")
     
+    #         final_data.append(data)
+    # save(final_data)
+    # return final_data
+    
+    for i in range(0,len(price)):
+        
+        if  src in price[i].lower():
+            
+            if price[i+4]=='+ 1 DAY':
+                data={
+                    'company':price[i-3],
+                    'flightNo':price[i-2],
+                    'depTime':price[i-1],
+                    'source':price[i],
+                    'duration':price[i+1],
+                    'type':price[i+2],
+                    'arrivalTime':price[i+3]+price[i+4],
+                    'destination':price[i+5],
+                    'rate':price[i+6]
+                }
+                
+            else:
+                data={
+                    'company':price[i-3],
+                    'flightNo':price[i-2],
+                    'depTime':price[i-1],
+                    'source':price[i],
+                    'duration':price[i+1],
+                    'type':price[i+2],
+                    'arrivalTime':price[i+3],
+                    'destination':price[i+4],
+                    'rate':price[i+5]
+                }
+            print("\n")
+            print(data)
             final_data.append(data)
-            save(final_data)
+    
+    save(final_data)
+    return final_data
+            
